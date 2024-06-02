ebe6024f5487e1e1091172f209dd1800494ffba2
SteveZhang <871506263@qq.com>
2024-06-03 00:07:20 +0800

    开启gzip压缩

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,13 +1,21 @@
 const { merge } = require('webpack-merge');
 const baseconfig = require('./webpack.base');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const CompressionPlugin = require('compression-webpack-plugin');
 console.log(`process.env.NODE_ENV:`, process.env.NODE_ENV);
 
 module.exports = merge(baseconfig, {
   mode: 'production',
   devtool: 'hidden-source-map',
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/, // 压缩 js, css, html, 和 svg 文件
+      threshold: 8, // 只处理大于这个大小的文件
+      minRatio: 0.8 // 压缩比例阈值
+    })
+  ],
   cache: {
     type: 'filesystem',
     buildDependencies: {
